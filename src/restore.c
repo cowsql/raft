bc17421b862bbcacbf97f1e86d31da673b716a0d
Free Ekanayaka <free@ekanayaka.io>
2023-05-21 22:10:08 +0100

    restore: Use unsigned to count entries
    Change restoreEntries() to use unsigned instead of size_t.
    
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- src/restore.c
+++ src/restore.c
@@ -64,11 +64,11 @@ static int restoreEntries(struct raft *r,
                           raft_term snapshot_term,
                           raft_index start_index,
                           struct raft_entry *entries,
-                          size_t n)
+                          unsigned n)
 {
     struct raft_entry *conf = NULL;
     raft_index conf_index = 0;
-    size_t i;
+    unsigned i;
     int rv;
     logStart(r->log, snapshot_index, snapshot_term, start_index);
     r->last_stored = start_index - 1;
@@ -226,7 +226,7 @@ int raft_start(struct raft *r)
      * configuration. */
     tracef("restore %zu entries starting at %llu", n_entries, start_index);
     rv = restoreEntries(r, snapshot_index, snapshot_term, start_index, entries,
-                        n_entries);
+                        (unsigned)n_entries);
     if (rv != 0) {
         entryBatchesDestroy(entries, n_entries);
         return rv;
