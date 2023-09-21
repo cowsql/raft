98dc66cb2bc9fea5b04caff66ba9d95cc883b339
Free Ekanayaka <free@ekanayaka.io>
2023-09-21 22:53:07 +0100

    legacy: Forward to `raft_io` tasks of type RAFT_SEND_MESSAGE
    Use the `raft_io->send()` API to execute tasks of type RAFT_SEND_MESSAGE.
    
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- src/legacy.c
+++ src/legacy.c
@@ -1,5 +1,6 @@
 #include "legacy.h"
 #include "assert.h"
+#include "err.h"
 
 static struct raft_event *eventAppend(struct raft_event *events[],
                                       unsigned *n_events)
@@ -15,6 +16,65 @@ static struct raft_event *eventAppend(struct raft_event *events[],
     *n_events += 1;
 
     return &(*events)[*n_events - 1];
+}
+
+/* Call LegacyForwardToRaftIo() after an asynchronous task has been
+ * completed. */
+static void ioTaskDone(struct raft *r, struct raft_task *task, int status)
+{
+    struct raft_event event;
+    event.type = RAFT_DONE;
+    event.time = r->io->time(r->io);
+    event.done.task = *task;
+    event.done.status = status;
+    LegacyForwardToRaftIo(r, &event);
+}
+
+struct ioForwardSendMessage
+{
+    struct raft_io_send send;
+    struct raft *r;
+    struct raft_task task;
+};
+
+static void ioSendMessageCb(struct raft_io_send *send, int status)
+{
+    struct ioForwardSendMessage *req = send->data;
+    struct raft *r = req->r;
+    struct raft_task task = req->task;
+    raft_free(req);
+    ioTaskDone(r, &task, status);
+}
+
+static int ioSendMessage(struct raft *r, struct raft_task *task)
+{
+    struct raft_send_message *params = &task->send_message;
+    struct ioForwardSendMessage *req;
+    struct raft_message message;
+    int rv;
+
+    req = raft_malloc(sizeof *req);
+    if (req == NULL) {
+        return RAFT_NOMEM;
+    }
+    req->r = r;
+    req->task = *task;
+    req->send.data = req;
+
+    message = params->message;
+    message.server_id = params->id;
+    message.server_address = params->address;
+
+    rv = r->io->send(r->io, &req->send, &message, ioSendMessageCb);
+    if (rv != 0) {
+        raft_free(req);
+        ErrMsgTransferf(r->io->errmsg, r->errmsg,
+                        "send message of type %d to %llu", params->message.type,
+                        params->id);
+        return rv;
+    }
+
+    return 0;
 }
 
 static int ioPersistTermAndVote(struct raft *r,
@@ -67,6 +127,8 @@ int LegacyForwardToRaftIo(struct raft *r, struct raft_event *event)
     }
 
     /* Initially the set of events contains only the event passed as argument,
+
+
      * but might grow if some of the tasks get completed synchronously. */
     events = raft_malloc(sizeof *events);
     if (events == NULL) {
@@ -92,6 +154,9 @@ int LegacyForwardToRaftIo(struct raft *r, struct raft_event *event)
             struct raft_task *task = &tasks[j];
 
             switch (task->type) {
+                case RAFT_SEND_MESSAGE:
+                    rv = ioSendMessage(r, task);
+                    break;
                 case RAFT_PERSIST_TERM_AND_VOTE:
                     rv = ioPersistTermAndVote(r, task, &events, &n_events);
                     break;
