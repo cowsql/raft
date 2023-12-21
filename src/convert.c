116f51377c3e2b525635d3d91d47b67b73d77e50
Free Ekanayaka <free@ekanayaka.io>
2023-12-21 14:32:24 +0000

    convert: Use the low-level ClientSubmit() instead of clientBarrier()
    The clientBarrier() helper actually duplicates ClientSubmit(), so just use it
    directly.
    
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- src/convert.c
+++ src/convert.c
@@ -238,18 +238,43 @@ int convertToLeader(struct raft *r)
          * term. Raft handles this by having each leader commit a blank no-op
          * entry into the log at the start of its term. */
         struct raft_barrier *req = raft_malloc(sizeof(*req));
+        struct raft_entry entry;
+        raft_index index;
+
         if (req == NULL) {
             return RAFT_NOMEM;
         }
-        rv = clientBarrier(r, req, convertInitialBarrierCb);
+
+        index = logLastIndex(r->log) + 1;
+        req->type = RAFT_BARRIER;
+        req->index = index;
+        req->cb = convertInitialBarrierCb;
+
+        entry.type = RAFT_BARRIER;
+        entry.term = r->current_term;
+        entry.buf.len = 8;
+        entry.buf.base = raft_malloc(entry.buf.len);
+
+        if (entry.buf.base == NULL) {
+            raft_free(req);
+            return RAFT_NOMEM;
+        }
+
+        rv = ClientSubmit(r, &entry, 1);
         if (rv != 0) {
             tracef(
                 "failed to send no-op barrier entry after leader conversion: "
                 "%d",
                 rv);
+            raft_free(entry.buf.base);
+            raft_free(req);
+            goto out;
         }
+
+        QUEUE_PUSH(&r->leader_state.requests, &req->queue);
     }
 
+out:
     return rv;
 }
 
