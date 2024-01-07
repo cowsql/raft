d0a66b7dca30e3ae48356b6e783ada57998314ab
Free Ekanayaka <free@ekanayaka.io>
2024-01-07 18:41:39 +0000

    test: Skip fuzzy tests when SKIP_FUZZY_TESTS is set
    This helps avoiding noise when generating code coverage.
    
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- test/fuzzy/main_core.c
+++ test/fuzzy/main_core.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "../lib/runner.h"
 
 MunitSuite _main_suites[64];
@@ -7,5 +9,8 @@ int _main_suites_n = 0;
 int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc)])
 {
     MunitSuite suite = {(char *)"", NULL, _main_suites, 1, 0};
+    if (getenv("SKIP_FUZZY_TESTS") != NULL) {
+        return 0;
+    }
     return munit_suite_main(&suite, (void *)"unit", argc, argv);
 }
