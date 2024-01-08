ecf3d1fde473862e34af8a9432480af0e1cfd52b
Free Ekanayaka <free@ekanayaka.io>
2024-01-08 19:05:43 +0000

    test: Drop unnecessary raft_apply/two
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- test/integration/test_apply.c
+++ test/integration/test_apply.c
@@ -110,19 +110,6 @@ static bool applyCbHasFired(struct raft_fixture *f, void *arg)
 
 SUITE(raft_apply)
 
-/* Append two command entries. */
-TEST(raft_apply, two, setUp, tearDown, 0, NULL)
-{
-    struct fixture *f = data;
-    int val = 123;
-    APPLY(0, val);
-    munit_assert_int(FsmGetX(CLUSTER_FSM(0)), ==, val);
-    val = 124;
-    APPLY(0, val);
-    munit_assert_int(FsmGetX(CLUSTER_FSM(0)), ==, val);
-    return MUNIT_OK;
-}
-
 /******************************************************************************
  *
  * Failure scenarios
