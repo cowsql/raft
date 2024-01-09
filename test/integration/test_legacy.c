19fcd9f71c13eff027503f381a6c0b6369df48c2
Free Ekanayaka <free@ekanayaka.io>
2024-01-09 19:12:56 +0000

    test: Move raft_recover() tests to test_legacy.c
    Signed-off-by: Free Ekanayaka <free@ekanayaka.io>

--- test/integration/test_legacy.c
+++ test/integration/test_legacy.c
@@ -318,3 +318,47 @@ TEST(raft_bootstrap, busy, setUpBootstrap, tearDownBootstrap, 0, NULL)
 
     return MUNIT_OK;
 }
+
+/******************************************************************************
+ *
+ * Recover tests.
+ *
+ *****************************************************************************/
+
+static void *setUpRecover(const MunitParameter params[],
+                          MUNIT_UNUSED void *user_data)
+{
+    struct fixture *f = munit_malloc(sizeof *f);
+    SETUP_CLUSTER(3);
+    CLUSTER_BOOTSTRAP;
+    return f;
+}
+
+static void tearDownRecover(void *data)
+{
+    struct fixture *f = data;
+    TEAR_DOWN_CLUSTER;
+    free(f);
+}
+
+SUITE(raft_recover)
+
+/* Attempting to recover a running instance results in RAFT_BUSY. */
+TEST(raft_recover, busy, setUpRecover, tearDownRecover, 0, NULL)
+{
+    struct fixture *f = data;
+    struct raft *raft;
+    struct raft_configuration configuration;
+    int rv;
+
+    /* Start all servers. */
+    CLUSTER_START();
+
+    raft = CLUSTER_RAFT(0);
+    CLUSTER_CONFIGURATION(&configuration);
+    rv = raft_recover(raft, &configuration);
+    munit_assert_int(rv, ==, RAFT_BUSY);
+    raft_configuration_close(&configuration);
+
+    return MUNIT_OK;
+}
